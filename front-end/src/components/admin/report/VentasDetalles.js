8d088a20a58ba2b4f2fc67aa26f821f1c422d93e
Oriel-Barroso <oriel_gold97@outlook.com>
2021-12-02 16:13:21 -0300

    ventas detalles front testing

--- front-end/src/components/admin/report/VentasDetalles.js
+++ front-end/src/components/admin/report/VentasDetalles.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Cookies from 'universal-cookie'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 
 export const VentasDetalles = () => {
@@ -11,8 +14,22 @@ export const VentasDetalles = () => {
     
     const [inputValue, setInputValue] = useState([]);
 
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
+
+    }
+
+    const handleChangeStart = (date) => {
+        setStartDate(date)
+        console.log(startDate.getDate(), 'dia')
+    }
+
+    const handleChangeEnd = (date) => {
+        setEndDate(date)
+        console.log(endDate.getFullYear(), 'dia fin')
     }
 
     const getVentas = () => {
@@ -22,21 +39,74 @@ export const VentasDetalles = () => {
         })
     }
 
-    useEffect(()=>{getVentas()},[])
+    useEffect(()=>{
+        getVentas()
+    },[])
 
-
-    const ventasFilter = () => {
-
+    const getVentasRango = async () => {
+        const fechaInicial = (startDate.getFullYear() + '-' + (startDate.getMonth()+1) + '-' + startDate.getDate()).toString()
+        const fechaFinal = (endDate.getFullYear() + '-' + (endDate.getMonth()+1) + '-' + endDate.getDate()).toString()
+        await axios.get(urlCDC+'/carro/ventasRangoFecha/'+ fechaInicial + '/' + fechaFinal + '/')
+        .then(res => {
+            console.log(res.data)
+            setventasDetalles(res.data)
+        })
     }
 
+    const getVentasAño = async () => {
+        await axios.get(urlCDC+'/carro/ventasAño/')
+        .then(res => {
+            console.log(res.data)
+            setventasDetalles(res.data)
+        })
+    }
+
+    const getVentasMesCorriente = async () => {
+        await axios.get(urlCDC+'/carro/ventasMesCorriente/')
+        .then(res => {
+            console.log(res.data)
+            setventasDetalles(res.data)
+        })
+    }
+
+    const getventasUltimosTreintaDias = async () => {
+        await axios.get(urlCDC+'/carro/ventasUltimosTreintaDias/')
+        .then(res => {
+            console.log(res.data)
+            setventasDetalles(res.data)
+        })
+    }
+
+
     return (
-        <form onSubmit={ventasFilter}>
-            Filter<input
-                type='text'
-                value={inputValue}
-                onChange={handleInputChange}
-            />
-        </form>
+        <>
+        <h1>Ventas</h1>
+        <DatePicker
+        type="text"
+        selected={startDate}
+        onChange={data=>handleChangeStart(data)}
+        value={startDate}
+        dateFormat="yyyy/MM/dd"/>
+        <DatePicker
+        type="text"
+        selected={endDate}
+        onChange={data=>handleChangeEnd(data)}
+        value={endDate}
+        dateFormat="yyyy/MM/dd"/>
+        <br/>
+        <button onClick={getVentasRango}>Por rango de fechas</button>
+        <br/>
+        <button onClick={getventasUltimosTreintaDias}>Ultimos 30 dias</button>
+        <br/>
+        <button onClick={getVentasMesCorriente}>Mes corriente</button>
+        <br/>
+        <button onClick={getVentasAño}>Ventas del año</button>
+        {<ul>
+            {ventasDetalles.map((ventas) => {
+                <li>{ventas.nombre}</li>
+            })}
+        </ul>}
+    </>
     )
 }
 
