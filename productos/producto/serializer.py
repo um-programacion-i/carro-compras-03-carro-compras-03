3eb7b71fdeb7d434cfe47d8f149eccd2f630a7ef
Oriel-Barroso <oriel_gold97@outlook.com>
2021-12-14 15:03:15 -0300

    version finalizada

--- productos/producto/serializer.py
+++ productos/producto/serializer.py
@@ -3,14 +3,17 @@ from rest_framework import serializers
 from .models import Producto, Distribuidor
 
 
-class ProductoSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Producto
-        fields = ['id', 'nombre', 'descripcion', 'precio', 'disponible',
-                  'idDistribuidor_id', 'cantidadVendido']
 
 
 class DistribuidorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Distribuidor
         fields = ['id', 'nombre', 'descripcion', 'disponible']
+
+
+
+class ProductoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Producto
+        fields = ['id', 'nombre', 'descripcion', 'precio', 'disponible',
+                  'idDistribuidor_id', 'cantidadVendido']
